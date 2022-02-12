0a1691c8807ab8587870482cd4b0fb7bf2104f7a
Saleh Suboh <subohsaleh@gmail.com>
2022-02-12 11:24:26 -0500

    modifying more files

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function compute() {
   document.getElementById("result").innerHTML =
     "If you deposit " +
     principal +
-    ",<br>at an interest rate of " +
+    ",at an interest rate of " +
     rate +
-    "%<br>You will receive an amount of " +
+    "% You will receive an amount of " +
     amount +
-    ",<br>in the year " +
+    ",in the year " +
     year +
-    "<br>";
+    "<br />";
 }
 
 function updateRate() {
